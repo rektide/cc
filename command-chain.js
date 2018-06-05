30d93c806965cee4ee2a17db78ed36c5b6c6de55
rektide de la faye <rektide@voodoowarez.com>
2018-06-04 20:16:15 -0400

    Create optional `.eval` and rename `.nextElement`.
    This allows more flexibility to users of command-chain.

--- command-chain.js
+++ command-chain.js
@@ -10,20 +10,19 @@ export function nextFn(ctx){
 * More like "advance and run": advance to the next function in the chain, & run it, passing in `ctx`.
 */
 export function next(){
-	const
-	  // iterate, retrieving our next function
-	  _nextFn= this._nextFn|| nextFn,
-	  // find next function
-	  fn= _nextFn( this)
+	// iterate, retrieving our next function or element
+	const el= (this.nextElement|| nextElement)( this)
 	// end execution if there's nothing here
-	if (!fn){
-		return this
+	if (!el){
+		return
 	}
 
 	// next we run the function.
-	fn( this)
-	// return this, allowing an eloquent programming style
-	return this 
+	if( this.eval){
+		this.eval( el)
+	}else{
+		el( this)
+	}
 }
 
 /**
